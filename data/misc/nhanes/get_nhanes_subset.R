d7e4e208d566df761c49f973c201fa4cfd46456d
Evan Ray <eray@mtholyoke.edu>
2018-09-06 11:45:15 -0400

    actual code used to get nhanes subset

--- data/misc/nhanes/get_nhanes_subset.R
+++ data/misc/nhanes/get_nhanes_subset.R
@@ -1,9 +1,12 @@
 library(tidyverse)
+library(NHANES)
 
 NHANES_subset <- NHANES %>%
   filter(SurveyYr == "2011_12") %>%
-  select(ID, Gender, Age, Race3, Education, MaritalStatus, HHIncome, Poverty, Weight, Length, Height, Diabetes, nPregnancies, nBabies, PregnantNow)
+  select(ID, Gender, Age, Race3, Education, MaritalStatus, HHIncome, Poverty, Weight, Length, Height, Diabetes, nPregnancies, nBabies, PregnantNow) %>%
+  mutate(ID = seq_len(nrow(.))) %>%
+  sample_n(nrow(.))
+
 colnames(NHANES_subset)[4] <- "Race"
 
-write_csv(NHANES_subset, "lecture/20180125_intro_to_r/nhanes.csv")
-
+write_csv(NHANES_subset, "data/misc/nhanes/nhanes.csv")
