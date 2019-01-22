d659bec161b293d925d1f8c1fc5e4d0662a16376
Evan Ray <eray@mtholyoke.edu>
2019-01-22 18:54:10 -0500

    fix sleuth3 data save

--- data/sleuth3/save-sleuth3-data.R
+++ data/sleuth3/save-sleuth3-data.R
@@ -2,4 +2,4 @@ library(Sleuth3)
 library(here)
 here()
 
-write.csv(Sleuth3::case0101, "data/sleuth3/case0101_creativity.csv")
+write.csv(Sleuth3::case0101, "data/sleuth3/case0101_creativity.csv", row.names = FALSE)
