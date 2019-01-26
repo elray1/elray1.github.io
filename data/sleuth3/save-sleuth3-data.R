7ab2b8b5c6b8c40f6c862f2129f5161d33bbaee9
Evan Ray <eray@mtholyoke.edu>
2019-01-26 14:44:11 -0500

    sleuth3 twins schizophrenia

--- data/sleuth3/save-sleuth3-data.R
+++ data/sleuth3/save-sleuth3-data.R
@@ -4,3 +4,4 @@ here()
 
 write.csv(Sleuth3::case0101, "data/sleuth3/case0101_creativity.csv", row.names = FALSE)
 write.csv(Sleuth3::ex0126, "data/sleuth3/ex0126_lcv_house.csv", row.names = FALSE)
+write.csv(Sleuth3::case0202, "data/sleuth3/ex0202_twins_schizophrenia.csv", row.names = FALSE)
