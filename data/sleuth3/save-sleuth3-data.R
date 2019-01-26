f982c54f8edfffe6b42b19b8a5f6982b9ae7d1a5
Evan Ray <eray@mtholyoke.edu>
2019-01-25 19:51:01 -0500

    sleuth3 ex0126 data

--- data/sleuth3/save-sleuth3-data.R
+++ data/sleuth3/save-sleuth3-data.R
@@ -3,3 +3,4 @@ library(here)
 here()
 
 write.csv(Sleuth3::case0101, "data/sleuth3/case0101_creativity.csv", row.names = FALSE)
+write.csv(Sleuth3::ex0126, "data/sleuth3/ex0126_lcv_house.csv", row.names = FALSE)
