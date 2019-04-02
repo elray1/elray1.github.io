c63ba28e53b89ecf74e8a74d4e8e0f006701c2fb
Evan Ray <eray@mtholyoke.edu>
2019-04-02 15:31:04 -0400

    sleuth3 ex1124 data

--- data/sleuth3/save-sleuth3-data.R
+++ data/sleuth3/save-sleuth3-data.R
@@ -23,4 +23,5 @@ write.csv(Sleuth3::ex0817, "data/sleuth3/ex0817_pest_control.csv", row.names = F
 write.csv(Sleuth3::ex0824, "data/sleuth3/ex0824_respiratory_rates.csv", row.names = FALSE)
 write.csv(Sleuth3::ex0825, "data/sleuth3/ex0825_2000_election.csv", row.names = FALSE)
 write.csv(Sleuth3::case0901, "data/sleuth3/case0901_meadowfoam.csv", row.names = FALSE)
+write.csv(Sleuth3::ex1124, "data/sleuth3/ex1124_natal_dispersion.csv", row.names = FALSE)
 
